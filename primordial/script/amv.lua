d2352734a50abbfd25dab86cb9006fb07359ca37
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-08-30 23:51:26 +0500

    Update amv.lua

--- primordial/script/amv.lua
+++ primordial/script/amv.lua
@@ -1,4 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BIudnyHoIandan/MM2/refs/heads/main/file.lua"))()
+
 -- Load WindUI safely
 local success, WindUI = pcall(function()
     print("Attempting to load WindUI...")
